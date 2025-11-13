b71f172e0a44162a7a178542bbf70bb8ec43c0dc
Acquani <alejandro.soto@cunef.edu>
2025-11-13 19:22:43 +0100

    Add files via upload
    Añado los métodos getter y el de mostrar información del circuito

--- Circuito.cpp
+++ Circuito.cpp
@@ -10,4 +10,27 @@ Circuito::Circuito(std::string nombre, std::string pais, int longitud, int vuelt
       vueltas(vueltas)
 {
 }
+//Getters
+std::string Circuito::getNombre() const {
+    return nombre;
+}
 
+std::string Circuito::getPais() const {
+    return pais;
+}
+
+int Circuito::getLongitud() const {
+    return longitud;
+}
+
+int Circuito::getVueltas() const {
+    return vueltas;
+}
+
+// Muestra la información del circuito por pantalla
+void Circuito::mostrarInfo() const {
+    std::cout << "Circuito: " << nombre << "\n";
+    std::cout << "País: " << pais << "\n";
+    std::cout << "Longitud: " << longitud << " km\n";
+    std::cout << "Vueltas: " << vueltas << "\n";
+}
