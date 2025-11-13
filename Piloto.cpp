c9fcb10898e8a065bbc1f3780ca5403f8e0bbf63
Alejandro Soto Acquani <alejandrosotoacquani@MacBook-Air-de-Alejandro-3.local>
2025-11-13 12:20:24 +0100

    subo el Piloto.cpp con el constructor creado

---
+++ Piloto.cpp
@@ -0,0 +1,11 @@
+// Piloto.cpp
+#include "Piloto.hpp"
+#include <string>
+// Constructor
+Piloto::Piloto(std::string nombre, int edad, std::string nacionalidad)
+    : nombre(nombre),
+      edad(edad),
+      nacionalidad(nacionalidad),
+      puntuacion()   // se llama al constructor por defecto de Puntuacion
+{
+}
